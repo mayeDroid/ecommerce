c5a56ced91728e2b2b24d6dfa9e5310f061eda59
Maye OIKEH <oiseomayeoikeh@outlook.com>
2023-05-06 08:22:21 +0100

    Created the UI for the fragment_home.xml

--- app/src/main/java/com/example/ecommerce/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/ecommerce/fragments/shopping/HomeFragment.kt
@@ -1,4 +1,6 @@
 package com.example.ecommerce.fragments.shopping
+//app:tabRippleColor="@color/white" the bubble effect when you click on the tab view
+// <androidx.viewpager2.widget.ViewPager2 helps shows selected fragment
 
 import androidx.fragment.app.Fragment
 import com.example.ecommerce.R
